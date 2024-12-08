0805d8d13e32413176854a7e42da20e193239e39
Subrat Shrestha <subrat.shrestha3@gmail.com>
2024-12-08 12:54:22 +1100

    day 7

--- src/day-7/day-7.ts
+++ src/day-7/day-7.ts
@@ -1,85 +1,75 @@
 import { readFileSync } from "fs";
 
-async function parseFileInput(): Promise<Array<[number, number[]]>> {
-  const text = readFileSync('./input.txt', 'utf8')
-  const lines = text.trim().split("\n");
-  return lines.map((line) => {
-    const [value, numbers] = line.split(":");
-    return [Number(value), numbers.trim().split(" ").map(Number)];
-  });
-}
+const lines = readFileSync('./input.txt', 'utf8').trim().split('\n');
+const input: [number, number[]][] = []
 
-async function part1() {
-  const equations = await parseFileInput();
-  let res = 0;
+lines.map(l => {
+    const [ans, nums] = l.split(':');
+    input.push([parseInt(ans), nums.trim().split(' ').map(n => parseInt(n.trim()))])
+})
 
-  for (const [value, numbers] of equations) {
-    if (isValidEquation(value, numbers, [add, multiply])) {
-      res += value;
-    }
-  }
-  console.log("Part 1:", res);
-}
+function isValid(lhs: number, nums: number[], operators: ((a: number, b: number) => number)[]): number {
+    let results: number[] = []
 
-async function part2() {
-  const equations = await parseFileInput();
-  let res = 0;
+    for (let i = 1; i < nums.length; i++) {
+        const tempResults: number[] = [];
 
-  for (const [value, numbers] of equations) {
-    if (isValidEquation(value, numbers, [add, multiply, concat])) {
-      res += value;
-    }
-  }
-  console.log("Part 2:", res);
-}
-
-/*
-190: 10 19
-3267: 81 40 27
-83: 17 5
-156: 15 6
-7290: 6 8 6 15
-161011: 16 10 13
-192: 17 8 14
-21037: 9 7 18 13
-292: 11 6 16 20
-*/
-
-function isValidEquation(value: number, numbers: number[], operators: ((a: number, b: number) => number)[]) {
-  let results: number[] = [];
-
-  for (let i = 1; i < numbers.length; i++) {
-    const newResults: number[] = [];
-
-    if (i === 1) {
-      for (const operator of operators) {
-        newResults.push(operator(numbers[i - 1], numbers[i]))
-      }
-    } else {
-      for (let j = 0; j < results.length; j++) {
-        for (const operator of operators) {
-          newResults.push(operator(results[j], numbers[i]))
+        if (i === 1) {
+            for (const operator of operators) {
+                tempResults.push(operator(nums[i-1], nums[i]))
+            }
+        } else {
+            for (let j = 0; j < results.length; j++) {
+                for (const operator of operators) {
+                    tempResults.push(operator(results[j], nums[i]))
+                }
+            }
         }
-      }
+        results = [...tempResults]
     }
-    results = [...newResults]
-  }
-  return results.some(result => result === value);
+
+    if (results.includes(lhs)) {
+        return lhs;
+    }
+
+    return 0
+}
+
+function part1() {
+    let res = 0;
+    const operators = [add, mul]
+
+    for (const [lhs, nums] of input) {
+        res += isValid(lhs, nums, operators)
+    }
+
+    console.log(res)
+}
+
+function part2() {
+    let res = 0;
+    const operators = [add, mul, concat]
+
+    for (const [lhs, nums] of input) {
+        res += isValid(lhs, nums, operators)
+    }
+
+    console.log(res)
 }
 
 function add(a: number, b: number) {
-  return a + b;
+    return a + b;
 }
 
-function multiply(a: number, b: number) {
-  return a * b;
+function mul(a: number, b: number) {
+    return a * b;
 }
 
 function concat(a: number, b: number) {
-  return Number(a.toString() + b.toString());
+    return parseInt(`${a}${b}`)
 }
 
-part1();
-part2();
+part1()
+part2()
 
 
